8072268a32e01995efa1044ea819b566edd276b4
Timur <chosti34@gmail.com>
2018-05-11 00:20:30 +0300

    Инициализирован проект для 5-ой лабораторной

---
+++ lw5/build.cmd
@@ -0,0 +1,65 @@
+@echo off
+
+if "%~1" == "" goto OnInvalidArgs
+
+set SEMVER_FOLDER_NAME="%~1"
+
+call :Build
+if %ERRORLEVEL% neq 0 goto OnBuildError
+
+call :Clear
+
+mkdir build\"%~1"\Frontend
+mkdir build\"%~1"\Frontend\wwwroot
+mkdir build\"%~1"\Backend
+mkdir build\"%~1"\TextRankCalc
+mkdir build\"%~1"\Config
+
+xcopy /q src\Frontend\bin\Release\netcoreapp2.0\publish build\"%~1"\Frontend > nul
+xcopy /q /s src\Frontend\wwwroot build\"%~1"\Frontend\wwwroot > nul
+xcopy /q src\Backend\bin\Release\netcoreapp2.0\publish build\"%~1"\Backend > nul
+xcopy /q src\TextRankCalc\bin\Release\netcoreapp2.0\publish build\"%~1"\TextRankCalc > nul
+xcopy /q src\Config build\"%~1"\Config > nul
+
+call :CreateRunScript
+call :CreateStopScript
+
+echo Project built and located at "build" folder
+exit /b 0
+
+:Clear
+	if exist build\%SEMVER_FOLDER_NAME% rd /s /q build\%SEMVER_FOLDER_NAME%
+	exit /b 0
+
+:Build
+	start /wait /d src\Frontend dotnet publish --configuration Release
+	if %ERRORLEVEL% neq 0 exit /b 1
+	start /wait /d src\Backend dotnet publish --configuration Release
+	if %ERRORLEVEL% neq 0 exit /b 1
+	start /wait /d src\TextRankCalc dotnet publish --configuration Release
+	if %ERRORLEVEL% neq 0 exit /b 1
+	exit /b 0
+
+:CreateRunScript
+	(
+		@echo start redis-server.exe
+		@echo start /d Frontend dotnet Frontend.dll
+		@echo start /d Backend dotnet Backend.dll
+		@echo start /d TextRankCalc dotnet TextRankCalc.dll
+	) > build\%SEMVER_FOLDER_NAME%\run.cmd
+	exit /b 0
+
+:CreateStopScript
+	(
+		@echo start taskkill /f /im dotnet.exe
+		@echo start taskkill /f /im redis-server.exe
+	) > build\%SEMVER_FOLDER_NAME%\stop.cmd
+	exit /b 0
+
+:OnInvalidArgs
+	echo Provide semver build version. Format: <major>.<minor>.<patch>
+	exit /b 1
+
+:OnBuildError
+	echo Failed to build project, try again later...
+	exit /b 1
---
+++ lw5/src/Backend/Controllers/ValuesController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using RabbitMQ.Client;
+using System.Text;
+using System.Threading;
+
+namespace Backend.Controllers
+{
+	[Route("api/[controller]")]
+	public class ValuesController : Controller
+	{
+		[HttpGet("{id}")]
+		public IActionResult Get(string id)
+		{
+			IDatabase database = GetRedisDatabaseConnection();
+			for (short i = 0; i < 5; ++i)
+			{
+				string rank = database.StringGet("TextRankGuid_" + id);
+				if (rank == null)
+				{
+					Thread.Sleep(200);
+				}
+				else
+				{
+					return Ok(rank);
+				}
+			}
+			return new NotFoundResult();
+		}
+
+		[HttpPost]
+		public string Post([FromForm]Models.StringHolder holder)
+		{
+			string id = Guid.NewGuid().ToString();
+			try
+			{
+				SaveKeyValuePairToRedis("TextContentGuid_" + id, holder.Data);
+				SendMessageToRabbitMQ("TextCreated_" + id);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			return id;
+		}
+
+		private void SendMessageToRabbitMQ(string message)
+		{
+			var factory = new ConnectionFactory();
+			using (var connection = factory.CreateConnection())
+			{
+				using (var channel = connection.CreateModel())
+				{
+					channel.QueueDeclare(
+						queue: "backend-api",
+						durable: false,
+						exclusive: false,
+						autoDelete: false,
+						arguments: null);
+
+					var bytes = Encoding.UTF8.GetBytes(message);
+					channel.BasicPublish(
+						exchange: "",
+						routingKey: "backend-api",
+						basicProperties: null,
+						body: bytes);
+
+					Console.WriteLine("Message: '" + message + "' sent to RabbitMQ");
+				}
+			}
+		}
+
+		private void SaveKeyValuePairToRedis(string key, string value)
+		{
+			IDatabase database = GetRedisDatabaseConnection();
+			database.StringSet(key, value);
+			Console.WriteLine("Pair (" + key + ", " + value + ") saved to redis");
+		}
+
+		private static IDatabase GetRedisDatabaseConnection()
+		{
+			ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+			return redis.GetDatabase();
+		}
+	}
+}
